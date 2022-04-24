c4fcbc78533d580a5dbc3404f43b67b29e92ee57
Dmitriy <94037443+ostermand@users.noreply.github.com>
2022-04-24 16:50:20 +0300

    Initial Commit

---
+++ Slider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Slider
+//
+//  Created by Дмитрий Остерман on 24.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
